4eb4e9e9ae72c53617c4f6eaba75913203af3ea4
GradySuchocki <157739832+GradySuchocki@users.noreply.github.com>
2024-03-07 14:13:38 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 import Demo from './Demo';
 import Demo2 from './Demo2';
-import StepsSlider from './component2';
+import {StepsSlider} from './component2';
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
